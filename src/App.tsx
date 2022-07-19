ad4505fcf285f7738c2732aa1b8142bd8507e819
Nailson97 <nailsonsousa97@gmail.com>
2022-07-19 19:18:50 -0300

    creating-form-structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import Modal from 'react-modal'
+import Modal from "react-modal";
 import { useState } from "react";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 
-Modal.setAppElement('#root')
+Modal.setAppElement("#root");
 
 export function App() {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -22,9 +23,10 @@ export function App() {
       <Header onOpenModal={handleOpenModal} />
       <Dashboard />
 
-      <Modal isOpen={isModalOpen} onRequestClose={handleCloseModal}>
-        <h2>Cadastrar transação</h2>
-      </Modal>
+      <NewTransactionModal
+        isOpen={isModalOpen}
+        onRequestClose={handleCloseModal}
+      />
       <GlobalStyle />
     </>
   );
