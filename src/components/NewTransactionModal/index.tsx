ff0e29212969db0f5914dc73851674661179a6da
Nailson97 <nailsonsousa97@gmail.com>
2022-07-28 16:44:47 -0300

    creating-button-functionality

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from "react-modal";
-import FecharImg from '../../assets/fechar.svg'
-import { Container } from "./styles";
+import FecharImg from "../../assets/fechar.svg";
+import EntradaImg from "../../assets/Entradas.svg";
+import SaidasImg from "../../assets/Saidas.svg";
+import { Container, TransactionTypeContainer } from "./styles";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -18,19 +20,35 @@ export function NewTransactionModal({
       overlayClassName="react-modal-overlay"
       className="react-modal-content"
     >
-      <button type='button'>
-        <img src={FecharImg} alt="Fechar modal" onClick={onRequestClose} className='react-modal-close' />
+      <button type="button">
+        <img
+          src={FecharImg}
+          alt="Fechar modal"
+          onClick={onRequestClose}
+          className="react-modal-close"
+        />
       </button>
       <Container>
         <h2>Cadastrar transação</h2>
-        <input placeholder="Título"/>
+        <input placeholder="Título" />
 
-        <input type="number" placeholder="Valor"/>
+        <input type="number" placeholder="Valor" />
 
-        <input placeholder="Categoria"/>
+        <TransactionTypeContainer>
+          <button type="button">
+            <img src={EntradaImg} alt="Entrada" />
+            <span>Entrada</span>
+          </button>
+
+          <button type="button">
+            <img src={SaidasImg} alt="Saida" />
+            <span>Saidas</span>
+          </button>
+        </TransactionTypeContainer>
+
+        <input placeholder="Categoria" />
 
         <button type="submit">Cadastrar</button>
-
       </Container>
     </Modal>
   );
