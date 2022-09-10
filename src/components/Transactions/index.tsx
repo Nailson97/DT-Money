09b4405d52301106b185ba5f9ee1553ac1a1dd44
Nailson97 <nailsonsousa97@gmail.com>
2022-09-10 10:14:04 -0300

    feat: listing transactions and seeds

--- src/components/Transactions/index.tsx
+++ src/components/Transactions/index.tsx
@@ -1,38 +1,47 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { api } from "../../services/api";
 import { Container } from "./styles";
 
+interface Transactions {
+    id: number,
+    title: string,
+    amount: number,
+    type: string,
+    category: string,
+    createdAt: string
+}
+
 export function Transaction() {
+    const [transactions, setTransactions ] = useState<Transactions[]>([])
+
     useEffect(() => {
         api.get('transactions')
-        .then(response => console.log(response.data))
+        .then(response => setTransactions(response.data.transactions))
     }, [])
     return (
-        <Container>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Título</th>
-                        <th>Valor</th>
-                        <th>Categoria</th>
-                        <th>Data</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td className="title">Criação de sites</td>
-                        <td className="deposit">R$8000,00</td>
-                        <td>Desenvolvimento</td>
-                        <td>12/07/2022</td>
-                    </tr>
-                    <tr>
-                        <td className="title">Aluguel</td>
-                        <td className="withdraw">- R$600,00</td>
-                        <td>Casa</td>
-                        <td>15/07/2022</td>
-                    </tr>
-                </tbody>
-            </table>
-        </Container>
-    )
+      <Container>
+        <table>
+          <thead>
+            <tr>
+              <th>Título</th>
+              <th>Valor</th>
+              <th>Categoria</th>
+              <th>Data</th>
+            </tr>
+          </thead>
+          <tbody>
+            {transactions.map((transaction) => {
+              return (
+                <tr key={transaction.id}>
+                  <td>{transaction.title}</td>
+                  <td className={transaction.type}>{transaction.amount}</td>
+                  <td>{transaction.category}</td>
+                  <td>{transaction.createdAt}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </Container>
+    );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,30 @@ createServer({
   models: {
     transaction: Model,
   },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Criação de Website',
+          type: 'deposit',
+          category: 'Dev',    
+          amount: 5200, 
+          createdAt: new Date('2022-09-10 09:25:00'),
+        },
+        {
+          id: 2,
+          title: 'Compras',
+          type: 'withdraw',
+          category: 'Gastos',    
+          amount: 2200, 
+          createdAt: new Date('2022-09-14 16:28:00'),
+        }
+      ]
+    })
+  },
+
   routes() {
     this.namespace = 'api';
 
